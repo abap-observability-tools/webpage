65377b84e299d0580dbfd1f4aa9e1d8d14a90873
Johannes Konings <johannes.konings@outlook.com>
2021-02-18 23:28:57 +0100

    new video for amp added (#40)
    * new video for amp added
    
    * change from p to br

--- src/components/Video.js
+++ src/components/Video.js
@@ -6,6 +6,10 @@ export default class Video extends React.Component {
         return (<div className="container container--md align-center">
             <ReactPlayer
                 url="https://www.youtube.com/watch?v=XV5brOMpzJA" />
+            <br />
+            <ReactPlayer
+                url="https://www.youtube.com/watch?v=K-e9gE_x4oU" />
+            <br />
         </div>)
     }
 }
