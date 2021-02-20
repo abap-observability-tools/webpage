d48fd769a17c4b3ecf730d5e72081626842f1f03
Johannes Konings <johannes.konings@outlook.com>
2021-02-20 15:37:01 +0100

    web monetization pointer added

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -7,18 +7,18 @@
 const React = require("react");
 const withPrefix = require("./src/utils/withPrefix").default;
 
-exports.onRenderBody = function({ setHeadComponents, setPostBodyComponents }) {
+exports.onRenderBody = function ({ setHeadComponents, setPostBodyComponents }) {
 
     setHeadComponents([
-        
+        <meta name="monetization" content="$ilp.uphold.com/7YxeW4A4NqQA"></meta>
     ]);
 
     setPostBodyComponents([
         <React.Fragment>
-            <script src={withPrefix('assets/js/init.js')}/>
-            <script src={withPrefix('assets/js/page-load.js')}/>
-            <script src={withPrefix('assets/js/page-unload.js')}/>
-            
+            <script src={withPrefix('assets/js/init.js')} />
+            <script src={withPrefix('assets/js/page-load.js')} />
+            <script src={withPrefix('assets/js/page-unload.js')} />
+
         </React.Fragment>
     ]);
 
