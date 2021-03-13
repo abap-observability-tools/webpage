534f0d07f1002d4750dbda53bcecbb6fc82383c9
Johannes Konings <johannes.konings@outlook.com>
2021-03-13 14:35:06 +0100

    update to gatsby v3

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
         `gatsby-plugin-react-helmet`,
         `gatsby-source-data`,
         `gatsby-transformer-remark`,
+        `gatsby-plugin-postcss`,
         {
             resolve: `gatsby-source-filesystem`,
             options: {
